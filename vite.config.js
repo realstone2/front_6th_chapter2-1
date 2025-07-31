1d6a345abe49efe1564abe6d8d51f1c721f41153
진돌 <wlstjrghdud@naver.com>
2025-08-01 05:44:36 +0900

    build(config): 빌드 설정 수정
    - base 경로와 copyPublicDir 옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/front_6th_chapter2-1/',
   build: {
     rollupOptions: {
       input: './index.html',
     },
     outDir: 'dist',
+    copyPublicDir: true,
   },
   test: {
     globals: true,
